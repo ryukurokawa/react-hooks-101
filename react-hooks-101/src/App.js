e8cb24f07c1a22b4eebd33ad24e3a7baf8c55952
ryukurokawa <setsuna1228@gmail.com>
2025-01-17 11:50:19 +0900

    implement object state

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,28 +1,31 @@
 import React, { useState } from 'react';
 
 const App = (props) => {
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
-
-  const reset = () => {
-    setPrice(props.price);
-  };
+  const [state, setState] = useState(props);
+  const { name, price } = state;
 
   return (
     <>
       <p>
-        現在の{name}は、{price}円です
+        現在の{state.name}は、{state.price}円です
       </p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => setState({ ...state, price: state.price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: state.price - 1 })}>
+        -1
+      </button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input
+        value={state.name}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      />
     </>
   );
 };
 
 App.defaultProps = {
-  name: 'サンプル',
+  name: '',
   price: 1000,
 };
 
