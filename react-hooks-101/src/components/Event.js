5143451d7d802d7c60826a82a221e772e713d09b
黒川颯 <ryu@kurokawasatsunoMacBook-Pro.local>
2025-01-20 16:52:43 +0900

    implement all events deletion feature

--- react-hooks-101/src/components/Event.js
+++ react-hooks-101/src/components/Event.js
@@ -4,7 +4,8 @@ import React from 'react'
 const Event = ({event, dispatch}) => {
   const id = event.id
   const handleClickDeleteButton = () => {
-    dispatch({type:'DELETE_EVENT', id})
+    const result = window.confirm(`イベント(id=${id})を本当に削除してもいいですか？`)
+    if (result) dispatch({type:'DELETE_EVENTS', id})
   }
   return(
     <tr>
