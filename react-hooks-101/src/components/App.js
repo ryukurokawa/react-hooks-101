c02e401f871e8c03d2c48656111394bce5881cc6
黒川颯 <ryu@kurokawasatsunoMacBook-Pro.local>
2025-01-20 21:08:48 +0900

    setup rteact context

--- react-hooks-101/src/components/App.js
+++ react-hooks-101/src/components/App.js
@@ -2,18 +2,21 @@ import React,{useReducer} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import EventForm from './EventForm'
-
 import Events from './Events'
+
+import AppContext from '../contexts/AppContext';
 import reducer from '../reducers'
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer,[])
 
   return (
-  <div className="container-fluid">
-    <EventForm state={state} dispatch={dispatch} />
-    <Events state={state} dispatch={dispatch} />
-  </div>
+    <AppContext.Provider value={"hello, I am provider"}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   )
 };
 
