ca611dba14b874bfce5a543dccbcef257ce7848a
黒川颯 <ryu@kurokawasatsunoMacBook-Pro.local>
2025-01-20 21:16:14 +0900

    soplve prop dorilling issue

--- react-hooks-101/src/components/Events.js
+++ react-hooks-101/src/components/Events.js
@@ -2,8 +2,8 @@ import React, {useContext} from "react"
 import Event from './Event'
 import AppContext from "../contexts/AppContext"
 
-const Events = ({state, dispatch}) => {
-const value = useContext(AppContext)
+const Events = () => {
+  const {state} = useContext(AppContext)
 
   return(
     <>
@@ -18,7 +18,7 @@ const value = useContext(AppContext)
         </tr>
       </thead>
       <tbody>
-      {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
+      {state.map((event, index) => (<Event key={index} event={event} />))}
       </tbody>
     </table>
     </>
