be0dcc580fba9eee6c982a9d47771e706dfcaac2
黒川颯 <ryu@kurokawasatsunoMacBook-Pro.local>
2025-01-22 11:55:49 +0900

    create-operationLogs-in-case-of-event-delation

--- react-hooks-101/src/components/Event.js
+++ react-hooks-101/src/components/Event.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
-import { DELETE_EVENT } from '../actions'
+import { ADD_OPERATION_LOG, DELETE_EVENT } from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
 
 
 const Event = ({event}) => {
@@ -8,8 +9,16 @@ const Event = ({event}) => {
   const id = event.id
   const handleClickDeleteButton = () => {
     const result = window.confirm(`イベント(id=${id})を本当に削除してもいいですか？`)
-    if (result) dispatch({type:DELETE_EVENT, id})
-  }
+    if (result){
+      dispatch({type:DELETE_EVENT, id})
+
+      dispatch({
+        type:ADD_OPERATION_LOG,
+        description:`イベント(id=${id})を削除しました。`,
+        operatedAt:timeCurrentIso8601()
+      })
+    }
+    } 
   return(
     <tr>
       <td>{event.id}</td>
